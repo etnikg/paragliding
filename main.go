c1496d3888ee6125baca8240da69a61af26581df
Etnik-Gashi <etnik.gashi@studentet.up-pr.edu>
2018-10-23 01:30:42 +0200

    DB side

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http" //"html/template"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -14,7 +14,207 @@ import (
 
 	"github.com/gorilla/mux"
 	igc "github.com/marni/goigc"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/objectid"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/findopt"
 )
+
+// *** MONGO *** //
+
+// ObjectID used in MongoDB
+type ObjectID [12]byte
+
+// Counter struct
+type Counter struct {
+	ID      objectid.ObjectID `bson:"_id"`
+	Counter int               `bson:"counter"`
+}
+
+func mongoConnect() *mongo.Client {
+	// Connect to MongoDB
+	conn, err := mongo.Connect(context.Background(), "mongodb://localhost:27017", nil)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return conn
+}
+
+// Check if the track already exists in the database
+func urlInMongo(url string, trackColl *mongo.Collection) bool {
+
+	// Read the documents where the trackurl field is equal to url parameter
+	cursor, err := trackColl.Find(context.Background(),
+		bson.NewDocument(bson.EC.String("url", url)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 'Close' the cursor
+	defer cursor.Close(context.Background())
+
+	track := Track{}
+
+	// Point the cursor at whatever is found
+	for cursor.Next(context.Background()) {
+		err = cursor.Decode(&track)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if track.Url == "" { // If there is an empty field, in this case, `url`, it means the track is not on the database
+		return false
+	}
+	return true
+}
+
+// Get trackName from URL
+func trackNameFromURL(url string, trackColl *mongo.Collection) string {
+	// Get the trackName
+	cursor, err := trackColl.Find(context.Background(),
+		bson.NewDocument(bson.EC.String("trackurl", url)))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(context.Background())
+
+	dbResult := Track{}
+
+	for cursor.Next(context.Background()) {
+		err = cursor.Decode(&dbResult)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// TODO : i changed from dbResult.TrackName to this... edit this accordingly
+	return dbResult.Url
+}
+
+// Get track counter from DB
+func getTrackCounter(db *mongo.Database) int {
+	counter := db.Collection("counter") // `counter` Collection
+
+	cursor, err := counter.Find(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(context.Background())
+
+	resCounter := Counter{}
+
+	for cursor.Next(context.Background()) {
+		err := cursor.Decode(&resCounter)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return resCounter.Counter
+}
+
+// Increase the track counter
+func increaseTrackCounter(cnt int32, db *mongo.Database) {
+	collection := db.Collection("counter") // `counter` Collection
+
+	// This is the way to update the counter field in the document
+	// Which is storen in the counter collection
+	_, err := collection.UpdateOne(context.Background(), nil,
+		bson.NewDocument(
+			bson.EC.SubDocumentFromElements("$set",
+				bson.EC.Int32("counter", cnt+1), // Increase the counter by one
+			),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Get all tracks
+func getAllTracks(client *mongo.Client, points bool) []Track {
+	db := client.Database("paragliding")  // `paragliding` Database
+	collection := db.Collection("tracks") // `track` Collection
+
+	var cursor mongo.Cursor
+	var err error
+	// If points boolean is true
+	// Get the points for the track also
+	// Otherwise don't
+	if points {
+		cursor, err = collection.Find(context.Background(), nil)
+	} else {
+		projection := findopt.Projection(bson.NewDocument(
+			bson.EC.Int32("trackpoints", 0),
+			bson.EC.Int32("_id", 0),
+		))
+
+		cursor, err = collection.Find(context.Background(), nil, projection)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(context.Background())
+
+	resTracks := []Track{}
+	resTrack := Track{}
+
+	for cursor.Next(context.Background()) {
+		err := cursor.Decode(&resTrack)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resTracks = append(resTracks, resTrack) // Append each resTrack to resTracks slice
+	}
+
+	return resTracks
+}
+
+// Get track
+func getTrack(client *mongo.Client, url string) Track {
+	db := client.Database("igcFiles")     // `paragliding` Database
+	collection := db.Collection("tracks") // `track` Collection
+
+	cursor, err := collection.Find(context.Background(), bson.NewDocument(bson.EC.String("url", url)))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resTrack := Track{}
+
+	for cursor.Next(context.Background()) {
+		err := cursor.Decode(&resTrack)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return resTrack
+
+}
+
+// Delete all tracks
+func deleteAllTracks(client *mongo.Client) {
+	db := client.Database("paragliding")  // `paragliding` Database
+	collection := db.Collection("tracks") // `track` Collection
+
+	// Delete the tracks
+	collection.DeleteMany(context.Background(), bson.NewDocument())
+
+	// Reset the track counter
+	increaseTrackCounter(int32(0), db)
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 var timeStarted = time.Now()
 
@@ -64,8 +264,10 @@ func trackLength(track igc.Track) float64 {
 
 //Track structure: This is structure for storing the track and to access their's id
 type Track struct {
-	ID       string    `json:"id"`
-	IgcTrack igc.Track `json:"igc_track"`
+	ID        string    `json:"id"`
+	Url       string    `json:"url"`
+	Timestamp string    `json:"timestamp"`
+	IgcTrack  igc.Track `json:"igc_track"`
 }
 
 //Attributes : the info about each igc file via id
@@ -75,6 +277,7 @@ type Attributes struct {
 	Glider     string  `json:"glider"`
 	GliderID   string  `json:"glider_id"`
 	Length     float64 `json:"track_length"`
+	TrackUrl   string  `json:"track_src_url"`
 }
 
 //Calculating uptime based on ISO 8601
@@ -133,8 +336,25 @@ func handler2(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		ids := make([]string, 0)
 
-		for i := range igcFiles {
-			ids = append(ids, igcFiles[i].ID)
+		client := mongoConnect()
+
+		collection := client.Database("igcFiles").Collection("tracks")
+
+		cursor, err := collection.Find(context.Background(), nil, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 'Close' the cursor
+		defer cursor.Close(context.Background())
+		track := Track{}
+
+		// Point the cursor at whatever is found
+		for cursor.Next(context.Background()) {
+			err = cursor.Decode(&track)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ids = append(ids, track.ID)
 		}
 
 		json.NewEncoder(w).Encode(ids)
@@ -151,7 +371,7 @@ func handler2(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Error!! ", error)
 			return
 		}
-		rand.Seed(time.Now().UnixNano())
+		//rand.Seed(time.Now().UnixNano())
 
 		res, err := regexp.MatchString(pattern, URL.URL)
 		if err != nil {
@@ -159,40 +379,53 @@ func handler2(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		if res {
-			mapID = searchMap(urlMap, URL.URL)
+			//mapID = searchMap(urlMap, URL.URL)
 			ID := rand.Intn(1000)
 
 			track, _ := igc.ParseLocation(URL.URL)
-			if mapID == -1 {
-				if findIndex(urlMap, initialID) {
-					uniqueId = ID
-					urlMap[uniqueId] = URL.URL
-					igcFile := Track{}
-					igcFile.ID = strconv.Itoa(ID)
-					igcFile.IgcTrack = track
 
-					igcFiles = append(igcFiles, igcFile)
+			//uniqueId = ID
+			//urlMap[uniqueId] = URL.URL
+			igcFile := Track{}
+			igcFile.ID = strconv.Itoa(ID)
+			igcFile.IgcTrack = track
+			igcFile.Url = URL.URL
 
-					json.NewEncoder(w).Encode(igcFile.ID)
-					return
-				} else {
-					rand.Seed(time.Now().UnixNano())
-					uniqueId = rand.Intn(1000)
-					urlMap[uniqueId] = URL.URL
-					igcFile := Track{}
-					igcFile.ID = strconv.Itoa(ID)
-					igcFile.IgcTrack = track
-					igcFiles = append(igcFiles, igcFile)
-					json.NewEncoder(w).Encode(igcFile.ID)
-					return
+			igcFile.Timestamp = time.Now().String()
 
+			client := mongoConnect()
+
+			collection := client.Database("igcFiles").Collection("tracks")
+
+			// Checking for duplicates so that the user doesn't add into the database igc files with the same URL
+			duplicate := urlInMongo(igcFile.Url, collection)
+
+			if !duplicate {
+
+				res, err := collection.InsertOne(context.Background(), igcFile)
+				if err != nil {
+					log.Fatal(err)
 				}
+
+				id := res.InsertedID
+
+				if id == nil {
+					http.Error(w, "", 300)
+				}
+
+				// Encoding the ID of the track that was just added to DB
+				json.NewEncoder(w).Encode(igcFile.ID)
+
 			} else {
-				uniqueId = searchMap(urlMap, URL.URL)
-				json.NewEncoder(w).Encode(uniqueId)
+
+				trackInDB := getTrack(client, igcFile.Url)
+				// If there is another file in igcFilesDB with that URL return and tell the user that that IGC FILE is already in the database
+				http.Error(w, "409 Conflict - The Igc File you entered is already in our database!", http.StatusConflict)
+				fmt.Fprintln(w, "\nThe file you entered has the following ID: ", trackInDB.ID)
 				return
 
 			}
+
 		}
 	default:
 		http.Error(w, "Not implemented", http.StatusNotImplemented)
@@ -281,18 +514,21 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/paragliding", handler)
 	r.HandleFunc("/paragliding/api", handler1)
-	r.HandleFunc("/paragliding/api/igc", handler2)
-	r.HandleFunc("/paragliding/api/igc/{id}", handler3)
-	r.HandleFunc("/paragliding/api/igc/{id}/{field}", handler4)
+	r.HandleFunc("/paragliding/api/track", handler2)
+	r.HandleFunc("/paragliding/api/track/{id}", handler3)
+	r.HandleFunc("/paragliding/api/track/{id}/{field}", handler4)
+	//r.HandleFunc("/paragliding/api/ticker/latest", getApiTickerLatest)
+	//r.HandleFunc("/paragliding/api/ticker/", getA)
+	//r.HandleFunc("/paragliding/api/ticker/{timestamp}", handler2)
 
 	//fmt.Println("listening...")
-
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
 	/*
-		if err := http.ListenAndServe(":8080", r); err != nil {
-			log.Fatal(err)
+		err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
 		}*/
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
